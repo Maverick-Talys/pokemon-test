83c16293292374e11a594dd19d6f2f5bdbebe83e
Maverick-Talys <alex.ray.dupont@gmail.com>
2025-01-23 10:16:44 +0100

    feat(API): add search bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,20 @@ import { useState, useEffect } from "react";
 const GRAPHQL_API = 'https://graphql-pokeapi.graphcdn.app/';
 
 
-const query = `
+
+
+
+export default function Home() {
+
+  const [result, setResult] = useState(null)
+
+
+
+  function request(searchName) {
+
+    const query = `
   {
-  pokemon(name: "pikachu") {
+  pokemon(name: ${searchName}) {
     name
     id
     abilities {
@@ -21,28 +32,20 @@ const query = `
 }
 `;
 
-
-export default function Home() {
-
-  const [result, setResult] = useState(null)
-
-  useEffect(() => {
     axios.post(GRAPHQL_API, { query })
       .then(response => {
-        console.log(response.data.pokemon);
-        setResult(response.data.pokemon)
+        console.log(JSON.stringify(response, null, 2));
+        setResult(response.data.data.pokemon)
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
-  }, [])
+  }
 
   console.log("FLAG", result)
 
   return (
     <div className="w-full">
-
-      <p>Loading ...</p>
 
       <main className="flex gap-8 items-center sm:items-start w">
         {/* Left bar */}
@@ -50,8 +53,10 @@ export default function Home() {
 
         </div>
         {/* main */}
-        <div className="w-3/5 border border-red-500 min-h-screen">
-          <p>{result?.data.pokemon.name}</p>
+        <div className="w-3/5 border border-red-500 min-h-screen justify-center">
+          <label for="pokemon-search">Search pokemon:</label>
+          <input type="search" id="pokemon-search" name="search-pokemon" onBlur={(v) => request(v.target.value)} />
+          <p>Name : {result.name}</p>
         </div>
       </main>
     </div>
